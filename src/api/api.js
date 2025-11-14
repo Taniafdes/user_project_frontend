4dfb89a55b9328522ba88536e1fed74e5f232d87
Taniafdes <82999608+Taniafdes@users.noreply.github.com>
2025-11-14 09:03:16 +0530

    Update API_URL to production endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://userprojectbackend-production.up.railway.app/api";
 
 // Auth APIs
 export const registerUser = (data) => axios.post(`${API_URL}/auth/register`, data);
